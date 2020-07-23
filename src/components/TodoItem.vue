129240f2e5637f59fe30bc8e42d67f095419df90
Ivan <iva.drakon.nov@gmail.com>
2020-07-23 13:11:31 +0200

    Todo Component

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -2,9 +2,14 @@
   <div class="todo">
     <p class="todo__info">
       <span class="todo__id">#{{todo.id + 1}}</span>
-      <span :class="['todo__status', (todo.done)?('todo__done'):('')]">{{(todo.done)?('выполенено'):('в очереди')}}</span>
+      <span class="todo__title">{{todo.title}}</span>
     </p>
     <p class="todo__body">{{todo.body}}</p>
+    <p class="todo__status">
+      <span :class="(todo.done)?('todo__done'):('')">{{(todo.done)?('выполенено'):('в очереди')}}</span>
+      <button class="todo__button todo__edit">edit</button>
+      <button class="todo__button todo__delete">delete</button>
+    </p>
   </div>
 </template>
 
@@ -28,7 +33,6 @@ export default {
   color: #342734;
 }
 .todo__id {
-  font-size: 18px;
   padding-right: 10px;
 }
 .todo__body {
@@ -37,14 +41,29 @@ export default {
   margin: 0;
   text-align: justify;
 }
-.todo__info {
+.todo__info, .todo__status{
   padding: 0;
   height: 24px;
   margin: 0;
-  background-color: #d6f6f9
+  font-size: 18px;
+  background-color: #d6f6f9;
+  text-align: left;
 }
 .todo__done {
   color: #78de67;
+  text-transform: uppercase;
 }
-
+.todo__title {
+  color: #672356;
+}
+.todo__button {
+  color: #256958;
+  text-transform: uppercase;
+  text-decoration: underline;
+  margin: 0;
+  padding: 0 10px;
+  background-color: transparent;
+  border: none;
+  float: right;
+}
 </style>
